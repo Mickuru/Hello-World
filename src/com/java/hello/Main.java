1d331d4bbd1db67d41ec1325e9bb9f42b2b29e0e
Alena <Valiullina.alena@mail.ru>
2021-01-13 18:18:57 +0400

    Hello

---
+++ src/com/java/hello/Main.java
@@ -0,0 +1,13 @@
+package com.java.hello;
+
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+
+
+    }
+}
